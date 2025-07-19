90729b5f56c07ca97f734231ab6b0aee17b2595f
Rishav Saigal <rishavsaigal@gmail.com>
2025-07-20 05:21:31 +0530

    Graph agent created need to test the flow

--- src_v2/graph.py
+++ src_v2/graph.py
@@ -1,10 +1,36 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from langgraph.graph import StateGraph
 from langchain_core.runnables import RunnableConfig
 from langgraph.checkpoint.memory import MemorySaver
 from utils.helper import AgentState, ConfigSchema
-from utils.agents import llm_agent
+from utils.agents import llm_agent, pandas_agent, python_agent
 
 graph = StateGraph(AgentState, config_schema=ConfigSchema)
 
 
+graph.add_node("Metadata Extractor Agent", llm_agent)
+graph.add_node("Pandas Agent", pandas_agent)
+graph.add_node("Structured Generator Agent", llm_agent)
+graph.add_node("Statistician Agent", llm_agent)
+graph.add_node("Python Coder Agent", python_agent)
+graph.add_node("Business Insight Agent", llm_agent)
+graph.add_node("Web Developer Agent", llm_agent)
+
+graph.add_edge("Metadata Extractor Agent", "Pandas Agent")
+graph.add_edge("Pandas Agent", "Structured Generator Agent")
+graph.add_edge("Structured Generator Agent", "Statistician Agent")
+graph.add_edge("Statistician Agent", "Python Coder Agent")
+graph.add_edge("Python Coder Agent", "Business Insight Agent")
+graph.add_edge("Business Insight Agent", "Web Developer Agent")
+
+graph.set_entry_point("Metadata Extractor Agent")
+graph.set_finish_point("Web Developer Agent")
+
+graph_agent = graph.compile()
+
+with open('mermaid_graph.png', 'wb') as f:
+    f.write(graph_agent.get_graph().draw_mermaid_png())
 
--- src_v2/utils/helper.py
+++ src_v2/utils/helper.py
@@ -1,10 +1,9 @@
 from enum import Enum
 from typing import Optional
-from typing_extensions import TypedDict
+from typing_extensions import TypedDict, Literal
 from langchain.chat_models import init_chat_model
 import pandas as pd
-
-from utils.helper import ModelClasses
+from langgraph.graph import END
 
 
 class ModelClasses(Enum):
@@ -34,11 +33,9 @@ class AgentState(TypedDict):
     State for the agent graph.
     """
     task: list | str
+    metadata: Optional[dict] = None
+    statistics: Optional[dict] = None
     df: pd.DataFrame
-    type_of_modelling: str
-    model_class: ModelClasses
-    target_column: Optional[str] = None
-    n_steps: int = 10
     stage: WorkflowStage
     history: list
 
@@ -59,3 +56,12 @@ def get_model(temperature: float = 1.0):
         model_provider="google_genai",
         temperature=temperature
     )
+
+STAGE_MAPPER = {
+    WorkflowStage.METADATA_EXTRACTOR_AGENT: WorkflowStage.PANDAS_AGENT,
+    WorkflowStage.PANDAS_AGENT: WorkflowStage.STRUCTURE_CREATOR_AGENT,
+    WorkflowStage.STRUCTURE_CREATOR_AGENT: WorkflowStage.STATISTICS_GENERATOR_AGENT,
+    WorkflowStage.STATISTICS_GENERATOR_AGENT: WorkflowStage.PYTHON_CODER_AGENT,
+    WorkflowStage.PYTHON_CODER_AGENT: WorkflowStage.BUSINESS_INSIGHTS_AGENT,
+    WorkflowStage.BUSINESS_INSIGHTS_AGENT: WorkflowStage.WEB_DEVELOPER_AGENT
+}
